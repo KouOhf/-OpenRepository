bfe6ec153ab7526c2a07fc11343bcd165d90028b
KouOhf <khy.ohf@outlook.jp>
2022-04-14 19:01:30 +0900

    P117まで

---
+++ spring-mvc2/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.model.Inquiry;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String config(@Validated @ModelAttribute Inquiry inquiry, BindingResult result) {
+		if(result.hasErrors()) {
+			return "index";
+		}
+		return "confirm";
+	}
+}
