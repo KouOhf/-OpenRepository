6d783da42138ae39e12e3465c1c86ce3882e6475
DesktopUser <khy.ohf@outlook.jp>
2022-04-18 02:04:48 +0900

    spring-jpa2-完成

---
+++ spring-jpa2/src/main/java/com/example/demo/config/DataLoader.java
@@ -0,0 +1,27 @@
+package com.example.demo.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Comment;
+import com.example.demo.repository.CommentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements ApplicationRunner{
+
+		private final CommentRepository repository;
+		
+		@Override
+		public void run(ApplicationArguments args) throws Exception{
+			var comment = new Comment();
+			comment.setContent("こんにちは");
+			repository.save(comment);
+			var testCommnet = new Comment();
+			testCommnet.setContent("テストコメント");
+			repository.save(testCommnet);
+		}
+}
