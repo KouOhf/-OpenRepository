1a311c1f798f122aea2c35df28240bd1313ef0db
KouOhf <khy.ohf@outlook.jp>
2022-05-07 06:23:08 +0900

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/KouOhf/-OpenRepository.git

---
+++ Spring_test_1/src/main/java/com/example/controller/CustomerController.java
@@ -0,0 +1,83 @@
+package com.example.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.entity.Customer;
+import com.example.form.CustomerForm;
+import com.example.service.CustomerService;
+
+
+@Controller
+public class CustomerController {
+    @Autowired
+    CustomerService customerService;
+ 
+    @GetMapping
+    String list(Model model) {
+      
+        List<Customer> customers = customerService.findAll();
+        model.addAttribute("customers", customers);
+        return "list";
+    }
+    
+    @GetMapping("create")
+    String create(@ModelAttribute CustomerForm customerForm) {
+        return "create";
+    }
+    
+    @PostMapping("create")
+    String regist(@ModelAttribute @Validated CustomerForm customerForm, BindingResult result, Model model) {
+        
+    	if (result.hasErrors()) {
+            return create(customerForm);
+        }
+    	
+    	Customer customer = new Customer();
+    	BeanUtils.copyProperties(customerForm, customer);
+    	customerService.insert(customer);
+    	return "redirect:/";
+    }
+    
+    @PostMapping(path = "edit", params = "edit")
+    String edit(@RequestParam Integer id, @ModelAttribute CustomerForm customerForm) {
+    	Optional<Customer> customerOpt = customerService.selectById(id);
+    	Customer customer = customerOpt.get();
+    	BeanUtils.copyProperties(customer, customerForm);
+    	return "edit";
+    }
+    
+    @PostMapping(path = "edit", params = "regist")
+    String regist(@RequestParam Integer id, @Validated @ModelAttribute CustomerForm customerForm, BindingResult result) {
+    	
+    	if(result.hasErrors()) {
+    		return edit(id, customerForm);
+    	}
+    	Customer customer = new Customer();
+    	BeanUtils.copyProperties(customerForm, customer);
+    	customerService.update(customer);
+    	return "redirect:/";
+    }
+    
+    @PostMapping(path = "edit", params = "back")
+    String back() {
+    	return "redirect:/";
+    }
+    
+    @PostMapping(path = "edit", params = "delete")
+    String delete(@RequestParam Integer id) {
+    	customerService.delete(id);
+    	return "redirect:/";
+    }
+}
---
+++ Spring_test_1/src/main/java/com/example/service/CustomerService.java
@@ -0,0 +1,39 @@
+package com.example.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.entity.Customer;
+import com.example.repository.CustomerRepository;
+
+//ポイント1
+@Service
+@Transactional
+public class CustomerService {
+    @Autowired
+    CustomerRepository customerRepository;
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    public void insert(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public void update(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public void delete(Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    public Optional<Customer> selectById(Integer id) {
+        return customerRepository.findById(id);
+    }
+}
