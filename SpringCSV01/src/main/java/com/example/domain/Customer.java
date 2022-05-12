17e5e19f8516d759cb8ae5c0c6cb1201758c915d
KouOhf <khy.ohf@outlook.jp>
2022-05-12 15:35:36 +0900

    springCSV-01

---
+++ SpringCSV01/src/main/java/com/example/domain/Customer.java
@@ -0,0 +1,29 @@
+package com.example.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String email;
+    
+    private String gender;
+}
